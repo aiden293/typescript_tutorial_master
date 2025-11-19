2ef6ddba79b7fb921636c87d67f4e4bd1315322a
SangHyup Lee <dunanob393@gmail.com>
2025-11-18 23:31:39 -0500

    intro file

---
+++ intro.js
@@ -0,0 +1,3 @@
+var user = { name: "Sang Lee", age: 25 };
+console.log("Sang Lee");
+console.log(user.name);
---
+++ intro.ts
@@ -0,0 +1,4 @@
+let user = {name: "Sang Lee", age: 25};
+
+console.log("Sang Lee");
+console.log(user.name);
